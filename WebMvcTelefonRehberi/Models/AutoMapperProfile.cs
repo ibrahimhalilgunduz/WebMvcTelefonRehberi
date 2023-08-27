056884041db5323c492bc19210f5abebfe9c0ee8
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-08-27 23:24:53 +0300

    KisiCreateDto Eklendi.

---
+++ WebMvcTelefonRehberi/Models/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebMvcTelefonRehberi.Domain;
+using WebMvcTelefonRehberi.Models.Dtos;
+
+namespace WebMvcTelefonRehberi.Models
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Kisi, KisiCreateDto>();
+            CreateMap<KisiCreateDto, Kisi>();
+        }
+    }
+}
---
+++ WebMvcTelefonRehberi/Models/Dtos/KisiCreateDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebMvcTelefonRehberi.Models.Dtos
+{
+    public class KisiCreateDto
+    {
+        [Required(ErrorMessage = "Ad alani Boş Geçilemez")]
+        [MaxLength(30, ErrorMessage = "Ad alani 30 karakterden buyuk olamaz.")]
+        public string Adi { get; set; }
+
+
+        [Required(ErrorMessage = "Soyad alani Boş Geçilemez")]
+        [MaxLength(50, ErrorMessage = "Soyad alani 50 karakterden buyuk olamaz.")]
+        public string Soyadi { get; set; }
+
+
+
+
+        [MaxLength(50, ErrorMessage = "Email alani 50 karakterden buyuk olamaz.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Gsm zorunlu alandir")]
+        [DataType(DataType.PhoneNumber)]
+        public string Gsm { get; set; }
+
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreateDate { get; set; }
+
+
+    }
+}
