a3ffedcac58f4213531211effea6111f491970d0
ibrahimhalilgunduz <ibrahimhalilgunduzz@gmail.com>
2023-08-27 23:24:53 +0300

    BootStrap Configurasyonu yapildi

--- WebMvcTelefonRehberi/Startup.cs
+++ WebMvcTelefonRehberi/Startup.cs
@@ -99,7 +99,7 @@ namespace WebMvcTelefonRehberi
 
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Kisis}/{action=Index}/{id?}");
             });
         }
     }
